33cfae8ca4c789e0ead99d2ec430ab158bf6759f
Hamidkhan-web <hamidulislamkhan2@gmail.com>
2020-08-06 23:58:04 +0600

    true vs folse

---
+++ Truthy-and-Falsy.js
@@ -0,0 +1,19 @@
+//folse ai golo
+//0;
+//folse
+//"";
+//undefined
+//null
+//NaN
+//tRUTHY...............
+// '0'  ' ' [] {}
+//'false'
+//const name = "solaiman";
+//console.log(name);
+let name = 12;
+if (name || name == 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}

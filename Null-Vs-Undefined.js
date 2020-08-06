fb1e5f1afe0de927fec4b1c4ff017c6da4d7e9c5
Hamidkhan-web <hamidulislamkhan2@gmail.com>
2020-08-07 00:35:58 +0600

    Null Vs Undefined

---
+++ Null-Vs-Undefined.js
@@ -0,0 +1,19 @@
+//undefined.......
+let pakhi;
+function add(num1, num2) {
+    console.log(num1 + num2);
+//return na dela undefined dakaba
+//return desi kintu ke return sata na lekla undefined dakaba
+return
+}
+
+function add(num1, num2=10) { //defolt 10 nela ar undefined dakaba na
+    console.log(num1, num2)
+}
+const result = add(13,); //parameter pass na korla undefined dekhaba
+console.log(result);
+
+const premik = {name:"smart dude", phone:456789};//propartir modda kono obgact ka console log korla undefined dakaba
+console.log(premik.gf);
+let fun = undefined;
+console.log(fun);
